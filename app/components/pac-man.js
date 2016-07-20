9bf712586da333e1742c8260a62f3b1fe5a5cd3b
Xavi G <alexandro.x.guerrero@gmail.com>
2016-07-20 15:33:34 -0500

    Add scaaarrrryyyy ghooooostsss and refactor movement

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import SharedStuff from '../mixins/shared-stuff';
+import Movement from '../mixins/movement';
 import Pac from '../models/pac';
+import Ghost from '../models/ghost';
 import Level from '../models/level';
 import Level2 from '../models/level2';
 import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
 
-export default Ember.Component.extend(KeyboardShortcuts, SharedStuff, {
+export default Ember.Component.extend(KeyboardShortcuts, SharedStuff, Movement, {
   didInsertElement() {
     let level = Level2.create();
     this.set('level', level);
@@ -17,11 +19,37 @@ export default Ember.Component.extend(KeyboardShortcuts, SharedStuff, {
     });
     this.set('pac', pac);
 
+    let ghosts = level.get('startingGhosts').map((startingPosition) => {
+      return Ghost.create({
+        level: level,
+        x: startingPosition.x,
+        y: startingPosition.y,
+        pac: pac
+      });
+    });
+
+    // let ghost1 = Ghost.create({
+    //   level: level,
+    //   x: 0,
+    //   y: 0,
+    //   pac: pac,
+    // });
+    // let ghost2 = Ghost.create({
+    //   level: level,
+    //   x: 5,
+    //   y: 0,
+    //   pac: pac,
+    // });
+    //
+    // let ghosts = [ghost1, ghost2];
+    this.set('ghosts', ghosts);
+
     this.loop();
   },
 
   score: 0,
   levelNumber: 1,
+  lives: 3,
 
   drawWall(x, y) {
     let ctx = this.get('ctx');
@@ -73,26 +101,48 @@ export default Ember.Component.extend(KeyboardShortcuts, SharedStuff, {
 
       if(this.get('level').isComplete()) {
         this.incrementProperty('levelNumber');
+        this.get('level').restart();
         this.restart();
       }
     }
   },
 
+  collidedWithGhosts() {
+    return this.get('ghosts').any((ghost) => {
+      return ghost.x === this.get('pac.x') &&
+             ghost.y === this.get('pac.y');
+    });
+  },
+
   loop() {
     this.get('pac').move();
+    this.get('ghosts').forEach((ghost) => { ghost.move(); });
 
     this.processAnyPellets();
 
     this.clearScreen();
     this.drawGrid();
+
     this.get('pac').draw();
+    this.get('ghosts').forEach((ghost) => { ghost.draw(); });
+
+    if(this.collidedWithGhosts()) {
+      this.decrementProperty('lives');
+      this.restart();
+    }
 
     Ember.run.later(this, this.loop, 1000/60);
   },
 
   restart() {
+    if(this.get('lives') <= 0) {
+      this.set('lives', 3);
+      this.set('score', 0);
+      this.set('levelNumber', 1);
+      this.get('level').restart();
+    }
     this.get('pac').restart();
-    this.get('level').restart();
+    this.get('ghosts').forEach((ghost) => { ghost.restart(); });
   },
 
   keyboardShortcuts: {
--- app/models/pac.js
+++ app/models/pac.js
@@ -1,14 +1,10 @@
 import Ember from 'ember';
 import SharedStuff from '../mixins/shared-stuff';
+import Movement from '../mixins/movement';
 
-export default Ember.Object.extend(SharedStuff, {
+export default Ember.Object.extend(SharedStuff, Movement, {
   direction: 'down',
   intent: 'down',
-
-  x: null,
-  y: null,
-
-  level: null,
 
   draw(){
     let x = this.get('x');
@@ -26,45 +22,6 @@ export default Ember.Object.extend(SharedStuff, {
     } else {
       this.set('direction', intent);
     }
-  },
-
-  move() {
-    if(this.animationCompleted()){
-      this.finalizeMove();
-      this.changeDirection();
-    } else if(this.get('pac.direction') === 'stopped') {
-      this.changeDirection();
-    } else {
-      this.incrementProperty('frameCycle');
-    }
-  },
-
-  animationCompleted() {
-    return this.get('frameCycle') === this.get('framesPerMovement');
-  },
-
-  finalizeMove() {
-    let direction = this.get('direction');
-    this.set('x', this.nextCoordinate('x', direction));
-    this.set('y', this.nextCoordinate('y', direction));
-
-    this.set('frameCycle', 1);
-  },
-
-  pathBlockedInDirection(direction) {
-    let cellTypeInDirection = this.cellTypeInDirection(direction);
-    return Ember.isEmpty(cellTypeInDirection) || cellTypeInDirection === 1;
-  },
-
-  cellTypeInDirection(direction) {
-    let nextX = this.nextCoordinate('x', direction);
-    let nextY = this.nextCoordinate('y', direction);
-
-    return this.get(`level.grid.${nextY}.${nextX}`);
-  },
-
-  nextCoordinate(coordinate, direction) {
-    return this.get(coordinate) + this.get(`directions.${direction}.${coordinate}`);
   },
 
   restart() {
